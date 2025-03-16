b69a3cf9be4d35a59597e6143487200cadc9252e
advaitmehla <advait.mehla@gmail.com>
2025-03-16 11:35:08 +0530

    testing modular sh.nix

---
+++ sh.nix
@@ -0,0 +1,30 @@
+# modular zsh config
+{ config, pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      ".." = "cd ..";
+      astro = "conda activate astro";
+      gitpc = "ssh -x -oHostKeyAlgorithms=+ssh-dss -x -A growth-crest@117.252.85.44";
+      "rebuildhome" = "home-manager switch --flake ~/.dotfiles/.";
+      "rebuildsys" = "sudo nixos-rebuild switch --flake ~/.dotfiles/.";
+      conda = "micromamba";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "sudo" ]; # Only include plugins that are part of oh-my-zsh
+    };
+
+    initExtra = ''
+      source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+      source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+    '';
+  }; 
+
+}
