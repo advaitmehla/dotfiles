6f3d98b3438425b211538b19f53de5a1039de766
advaitmehla <advait.mehla@gmail.com>
2025-03-19 17:26:01 +0530

    added option to use unstable

--- flake.nix
+++ flake.nix
@@ -1,44 +1,65 @@
 {
+  description = "Initial flake!";
 
-    description = "Initial flake!";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    inputs = {
-        # give url to get packages - latest as of March 2025. Does this have to be updated later?
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-        home-manager.url = "github:nix-community/home-manager/release-24.11";
-        home-manager.inputs.nixpkgs.follows = "nixpkgs"; # make sure versions are same
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs"; # Ensure consistency with nixpkgs
     };
 
-    outputs = {self, nixpkgs, home-manager, ...}:
-    let
-        lib = nixpkgs.lib; # done to "pass" lib to outputs - otherwise lib.xyz wont work
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-        nixosConfigurations = {
-            # give it any name for your config - usually hostname
-            nixos = lib.nixosSystem {
-                inherit system;
-                modules = [ ./configuration.nix ];
-            };
-        };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.home-manager.follows = "home-manager";
+    };
+  };
 
-        homeConfigurations = {
-            # give it any name for your config - usually username
-            advait = home-manager.lib.homeManagerConfiguration {
-                inherit pkgs;
-                modules = [ ./home.nix ];
-            };
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... } @ inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    unstablePkgs = nixpkgs-unstable.legacyPackages.${system};
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      
+      modules = [
+        # Use unstable channel for KDE packages only
+        ({ config, pkgs, ... }: {
+          nixpkgs.overlays = [
+            (final: prev: {
+              kdePackages = nixpkgs-unstable.legacyPackages.${prev.system}.kdePackages;
+            })
+          ];
+        })
+        ./configuration.nix
+      ];
 
-        };
+      specialArgs = {
+        inherit unstablePkgs inputs;
+      };
     };
 
-# to update system 1) nix flake update
-#                  2) sudo nixos-rebuild switch --flake .
-#                  3) home-manager switch --flake .
+    homeConfigurations.advait = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
 
-# flake.lock permission issue:
-# sudo chown advait flake.lock
-# sudo chgrp users flake.lock
+      extraSpecialArgs = {
+        inherit unstablePkgs inputs;
+      };
+
+      modules = [ ./home.nix ];
+    };
+  };
+
+  # Update system:
+  # 1) nix flake update
+  # 2) sudo nixos-rebuild switch --flake .
+  # 3) home-manager switch --flake .
+
+  # Fix flake.lock permission issues:
+  # sudo chown advait flake.lock
+  # sudo chgrp users flake.lock
 }
--- home.nix
+++ home.nix
@@ -1,12 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, unstablePkgs, inputs, ... }:
 
 {
   imports = [
     ./sh.nix
   ];
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "advait";
   home.homeDirectory = "/home/advait";
 
@@ -29,34 +27,9 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/advait/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
