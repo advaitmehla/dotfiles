b31e1a889abdeca358cbba35da76e0009cb2b6e1
advaitmehla <advait.mehla@gmail.com>
2025-03-31 15:42:43 +0530

    switched to use zinit

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -3,9 +3,9 @@
 
 {
   home.packages = with pkgs; [
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-    zsh-powerlevel10k
+    # zsh-autosuggestions
+    # zsh-syntax-highlighting
+    # zsh-powerlevel10k
     # fzf-zsh
     fzf
     # zsh-fzf-tab
@@ -31,36 +31,38 @@
 
     initExtra = ''
       source ${pkgs.zinit}/share/zinit/zinit.zsh
-      source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-      source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-      
-      # source ~/.fzf-tab-completion/zsh/fzf-zsh-completion.sh
-      # bindkey '^I' fzf_completion
-    
 
-      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
-
-      autoload -Uz compinit && compinit
-
-      bindkey '^[[1;5A' history-search-backward
-      bindkey '^[[1;5B' history-search-forward
-
-      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
-      # zstyle ':completion:*:*:git:*' fzf-search-display true
-      # zstyle ':completion:*' menu no
-      # zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+      # Async plugin loading
+      zinit wait'0' lucid light-mode for \
+        zdharma-continuum/fast-syntax-highlighting \
+        zsh-users/zsh-autosuggestions \
+        zsh-users/zsh-syntax-highlighting
 
       zinit snippet OMZP::git
       zinit snippet OMZP::sudo
-      # zinit snippet OMZP::command-not-found
 
+      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+
+      autoload -Uz compinit && compinit -C
+
+      # Keybindings
+      bindkey '^[[1;5A' history-search-backward
+      bindkey '^[[1;5B' history-search-forward
+
+      # Completion styles
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+
+      # History settings
       HISTDUP=erase
-      eval "$(fzf --zsh)"
-      eval "$(zoxide init --cmd cd zsh)"
 
+      eval "$(fzf --zsh)"
+
+      # Load zoxide with custom 'cd' alias
+      eval "$(zoxide init --cmd cd zsh)"
     '';
+
 
     history = {
       size = 10000;
